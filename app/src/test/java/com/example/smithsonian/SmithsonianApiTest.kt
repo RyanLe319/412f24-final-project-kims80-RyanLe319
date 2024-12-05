7bc1fe708e49c2ab88e13e11f43e8d16e9be1c11
Josh Kim <kims80@wwu.edu>
2024-12-05 14:24:10 -0800

    Fixed issue with api

--- app/src/test/java/com/example/smithsonian/SmithsonianApiTest.kt
+++ app/src/test/java/com/example/smithsonian/SmithsonianApiTest.kt
@@ -9,9 +9,9 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class SmithsonianApiTest {
     @Test
-    fun addition_isCorrect() {
+    fun searchGeneralTest() {
         assertEquals(4, 2 + 2)
     }
 }
