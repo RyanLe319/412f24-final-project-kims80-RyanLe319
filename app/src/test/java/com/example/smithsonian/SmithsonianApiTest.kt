1a3457502ccd870ad75c1cc2ee05044ef5ba0f1a
Josh Kim <kims80@wwu.edu>
2024-12-05 15:46:32 -0800

    Added unit testing for api

--- app/src/test/java/com/example/smithsonian/SmithsonianApiTest.kt
+++ app/src/test/java/com/example/smithsonian/SmithsonianApiTest.kt
@@ -12,6 +12,33 @@ import org.junit.Assert.*
 class SmithsonianApiTest {
     @Test
     fun searchGeneralTest() {
-        assertEquals(4, 2 + 2)
+        val objList = SmithsonianApi.searchGeneral("art")
+        val obj = objList[0]
+        assert(obj.id == "ld1-1643399756728-1643399815679-0")
+        assert(obj.title == "Society of American Artists, Jury of 1890")
+        assert(obj.image == "https://ids.si.edu/ids/deliveryService?id=NPG-NPG_2009_7M-000001")
+        assert(obj.date == "1890")
+        assert(obj.name == "Unidentified Artist")
+    }
+
+    @Test
+    fun searchCategoryTest() {
+        val objList = SmithsonianApi.searchCategory("art", "art_design")
+        val obj = objList[0]
+        assert(obj.id == "ld1-1643399756728-1643399815679-0")
+        assert(obj.title == "Society of American Artists, Jury of 1890")
+        assert(obj.image == "https://ids.si.edu/ids/deliveryService?id=NPG-NPG_2009_7M-000001")
+        assert(obj.date == "1890")
+        assert(obj.name == "Unidentified Artist")
+    }
+
+    @Test
+    fun searchTermsTest() {
+        val objList = SmithsonianApi.searchTerms("date")
+        assert(objList[0] == "0-00s")
+        assert(objList[1] == "0?00s")
+        assert(objList[2] == "1000s")
+        assert(objList[3] == "100s")
+        assert(objList[4] == "1100s")
     }
 }
